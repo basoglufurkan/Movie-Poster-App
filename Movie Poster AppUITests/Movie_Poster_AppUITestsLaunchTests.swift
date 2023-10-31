9f2d07e10fdcdd95aab8bbd8f49856d4fbde01d1
Furkan BAŞOĞLU <furkan.basoglu@karaca.com>
2023-10-31 11:31:23 +0300

    Initial Commit

---
+++ Movie Poster AppUITests/Movie_Poster_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Movie_Poster_AppUITestsLaunchTests.swift
+//  Movie Poster AppUITests
+//
+//  Created by Furkan BAŞOĞLU on 31.10.2023.
+//
+
+import XCTest
+
+final class Movie_Poster_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
